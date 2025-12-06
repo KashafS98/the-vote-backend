4bb335a604bfdf20dee22a3331e724cc9f14167f
KashafS98 <kashaf.tshaikh@gmail.com>
2025-12-06 13:15:56 +0000

    update server listening code

--- app.js
+++ app.js
@@ -214,6 +214,6 @@ io.on("connection", (client) => {
 
 // **Railway port**
 const PORT = process.env.PORT;
-app.listen(Number(PORT) || 3000, "0.0.0.0", () => {
+server.listen(Number(PORT) || 3000, "0.0.0.0", () => {
   console.log("Server is running on port", PORT || 3000);
 });
